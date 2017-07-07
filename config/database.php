484df1dfab13d0230abd74a9fb0a415de8adab92
Max <maximilianoroballo@gmail.com>
2017-07-07 07:11:43 -0700

    commit do htaccess

--- config/database.php
+++ config/database.php
@@ -12,4 +12,17 @@ class Database {
         return $this->db;
     }
 
+    public function executeSql($sql, $params){
+        $counter = 0;
+
+        $query = self::getConnection()->prepare($sql);
+        
+        if(count($params) > 0) foreach($params as $param) {
+            $query->bindParam(++$counter, $param);
+        }
+
+        $query->execute();
+
+    }
+
 }
